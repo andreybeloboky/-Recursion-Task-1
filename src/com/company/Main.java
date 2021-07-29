bf7d57351643be46ebb98cc15127ddf2c76a38b5
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-07-29 20:48:59 +0300

    add method using recursion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
-	int A = scanner.nextInt();
-	int B = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        if (A < B) {
+            Recursion.methodReuse(A, B);
+        } else {
+            Recursion.methodReuse(B, A);
+        }
     }
 }
